d47cb85dae04b5a9908a9fa55a89a60f6ec8c2b3
Murilo Roque <muriloengqui@gmail.com>
2020-06-23 10:48:19 -0300

    refactor and fix ESlint errors

--- src/app/todoDom.js
+++ src/app/todoDom.js
@@ -12,68 +12,15 @@ export const createOptions = () => {
   }
 };
 
-const todoComplete = (button, elt, values) => {
-  button.addEventListener('click', () => {
-    if (values[values.indexOf(elt)].complete) {
-      values[values.indexOf(elt)].complete = false;
-      button.textContent = 'Not Complete';
-    } else {
-      values[values.indexOf(elt)].complete = true;
-      button.textContent = 'Complete';
-    }
-    localStorage.setItem('todos', JSON.stringify(values));
-  });
-};
-
-const deleteTodo = (button, elt, values) => {
-  button.addEventListener('click', () => {
-    values.splice(values.indexOf(elt), 1);
-    localStorage.setItem('todos', JSON.stringify(values));
-    window.location.reload();
-  });
-};
-
-const editTodo = (button, elt, values, todoList) => {
-  button.addEventListener('click', () => {
-    todoList.classList.add('d-none');
-
-    const projectList = document.getElementById('project-list');
-
-    projectList.classList.add('d-none');
-
-    const todoForm = document.getElementById('todo-form');
-
-    todoForm.classList.remove('d-none');
-    document.getElementById('title').value = elt.title;
-    document.getElementById('description').value = elt.description;
-    document.getElementById('date').value = elt.date;
-    document.getElementById('priority').value = elt.priority;
-    document.getElementById('project').value = elt.project;
-    editForm(values, elt);
-  });
-};
-
-export const createButtons = (elt, values, todoList, projectName) => {
+export const createButtonsDom = (elt, values, todoList, projectName) => {
   const content = document.createElement('div');
   const todoName = document.createElement('p');
   const button = document.createElement('button');
+  button.setAttribute('id', 'button-complete');
   const editButton = document.createElement('button');
+  editButton.setAttribute('id', 'button-edit');
   const deleteButton = document.createElement('button');
-
-  editButton.textContent = 'Edit';
-  deleteButton.textContent = 'Delete';
-
-  editTodo(editButton, elt, values, todoList);
-  deleteTodo(deleteButton, elt, values);
-
-  if (values[values.indexOf(elt)].complete) {
-    button.textContent = 'Complete';
-  } else {
-    button.textContent = 'Not Complete';
-  }
-
-  todoComplete(button, elt, values);
-
+  deleteButton.setAttribute('id', 'button-delete');
   const {
     title, description, date, priority,
   } = elt;
@@ -89,4 +36,12 @@ export const createButtons = (elt, values, todoList, projectName) => {
   content.appendChild(button);
   content.appendChild(editButton);
   content.appendChild(deleteButton);
+
+  editButton.textContent = 'Edit';
+  deleteButton.textContent = 'Delete';
+  if (values[values.indexOf(elt)].complete) {
+    button.textContent = 'Complete';
+  } else {
+    button.textContent = 'Not Complete';
+  }
 };
--- src/app/todos.js
+++ src/app/todos.js
@@ -1,4 +1,4 @@
-import { createButtons } from './todoDom';
+import { createButtonsDom } from './todoDom';
 
 class Todo {
   constructor(title, description, date, priority, project, complete) {
@@ -16,26 +16,6 @@ let todos = JSON.parse(window.localStorage.getItem('todos'));
 if (todos == null) {
   todos = [];
 }
-
-export const todoForm = () => {
-  document.getElementById('id-todo-form').reset();
-  const submit = document.getElementById('id-todo-form');
-
-  submit.onsubmit = () => {
-    const title = document.getElementById('title').value;
-    const description = document.getElementById('description').value;
-    const date = document.getElementById('date').value;
-    const priority = document.getElementById('priority').value;
-    const project = document.getElementById('project').value;
-    const todo = new Todo(title, description, date, priority, project, false);
-
-    todos.push(todo);
-    localStorage.setItem('todos', JSON.stringify(todos));
-
-    window.location.reload();
-    return false;
-  };
-};
 
 const editForm = (values, elt) => {
   const submit = document.getElementById('id-todo-form');
@@ -59,6 +39,57 @@ const editForm = (values, elt) => {
   };
 };
 
+const editTodo = (elt, values, todoList) => {
+  const button = document.getElementById('button-edit');
+  button.addEventListener('click', () => {
+    todoList.classList.add('d-none');
+
+    const projectList = document.getElementById('project-list');
+
+    projectList.classList.add('d-none');
+
+    const todoForm = document.getElementById('todo-form');
+
+    todoForm.classList.remove('d-none');
+    document.getElementById('title').value = elt.title;
+    document.getElementById('description').value = elt.description;
+    document.getElementById('date').value = elt.date;
+    document.getElementById('priority').value = elt.priority;
+    document.getElementById('project').value = elt.project;
+    editForm(values, elt);
+  });
+};
+
+const todoComplete = (elt, values) => {
+  const button = document.getElementById('button-complete');
+  button.addEventListener('click', () => {
+    if (values[values.indexOf(elt)].complete) {
+      values[values.indexOf(elt)].complete = false;
+      button.textContent = 'Not Complete';
+    } else {
+      values[values.indexOf(elt)].complete = true;
+      button.textContent = 'Complete';
+    }
+    localStorage.setItem('todos', JSON.stringify(values));
+  });
+};
+
+const deleteTodo = (elt, values) => {
+  const button = document.getElementById('button-delete');
+  button.addEventListener('click', () => {
+    values.splice(values.indexOf(elt), 1);
+    localStorage.setItem('todos', JSON.stringify(values));
+    window.location.reload();
+  });
+};
+
+const createButtons = (elt, values, todoList, projectName) => {
+  createButtonsDom(elt, values, todoList, projectName);
+  editTodo(elt, values, todoList);
+  deleteTodo(elt, values);
+  todoComplete(elt, values);
+};
+
 export const todoShow = (projectName) => {
   const todoList = document.getElementById('todo-list');
 
@@ -74,4 +105,24 @@ export const todoShow = (projectName) => {
       }
     });
   }
+};
+
+export const todoForm = () => {
+  document.getElementById('id-todo-form').reset();
+  const submit = document.getElementById('id-todo-form');
+
+  submit.onsubmit = () => {
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const date = document.getElementById('date').value;
+    const priority = document.getElementById('priority').value;
+    const project = document.getElementById('project').value;
+    const todo = new Todo(title, description, date, priority, project, false);
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    window.location.reload();
+    return false;
+  };
 };
