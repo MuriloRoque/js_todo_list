f51e03b12820b12419aae5215c8d2b65a44b8b4c
Murilo Roque <muriloengqui@gmail.com>
2020-06-24 06:44:59 -0300

    initial commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -34,7 +34,7 @@ module.exports = merge(common, {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.s(a|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader, // 3. Extract css into files
           'css-loader', // 2. Turns css into commonjs
